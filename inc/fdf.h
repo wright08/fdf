7444051e1e790dae3d472f6ec3c61de383b5560b
wright08 <wright08@vt.edu>
2019-05-25 19:40:33 -0700

    Fuck scratchpixel

--- inc/fdf.h
+++ inc/fdf.h
@@ -16,5 +16,6 @@ t_vec3	*vec3(double x, double y, double z);
 double	vec3_dot(const t_vec3 *a, const t_vec3 *b);
 double	vec3_length(const t_vec3 *v);
 void	vec3_normalize(t_vec3 *v);
+t_vec3	*vec3_cross(const t_vec3 *a, const t_vec3 *b);
 
 #endif
